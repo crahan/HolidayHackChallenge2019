241090bbd40a9ce5d4eb3ab28e7f0bba74381e54
Thomas Bouve <crahan@n00.be>
2020-01-04 18:48:48 +0100

    Added original scripts to files folder.

---
+++ docs/files/find_code.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Tangle Coalbox - Frosty Keypad challenge."""
+import itertools
+
+
+def is_prime(number):
+    """Verify if a number is a prime."""
+    return 2 in [number, 2**number % number]
+
+
+def main():
+    """Execute."""
+    digit_sets = [
+        ['1', '1', '3', '7'],
+        ['1', '3', '3', '7'],
+        ['1', '3', '7', '7']
+    ]
+
+    primes = []
+
+    for digits in digit_sets:
+        for subset in itertools.permutations(digits):
+            val = int(''.join(subset))
+            if is_prime(val) and val not in primes:
+                primes.append(val)
+                print(f'{val} is a prime number')
+
+
+if __name__ == "__main__":
+    main()
---
+++ docs/files/match_user_agents.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""Find matching user_agent strings."""
+
+
+def main():
+    """Execute."""
+    file_bad = 'IPs_bad.csv'
+    file_all = 'IPs_all.csv'
+    list_bad = []
+    list_all = []
+
+    # Read the full data log
+    with open(file_all) as fp:
+        line = fp.readline()
+
+        while line:
+            list_all.append(line.split('\t'))
+            line = fp.readline()
+
+    # Read the bad IP data and match on user_agent but only
+    # keep the results if less than 4 matches are found.
+    with open(file_bad) as fp:
+        line = fp.readline()
+
+        while line:
+            tmp = []
+            line_bad = line.split('\t')
+
+            for line_all in list_all:
+                if line_all[4] == line_bad[4]:
+                    tmp.append(line_all[0])
+
+            # Only add if less than 4 matches
+            if len(tmp) < 4:
+                list_bad.extend(tmp)
+
+            # Add the original IP as well
+            list_bad.append(line_bad[0])
+            line = fp.readline()
+
+    # Remove duplicates
+    list_bad = list(dict.fromkeys(list_bad))
+
+    # Tadaaaaa!
+    print(f'Found {len(list_bad)} IPs: {",".join(list_bad)}')
+
+
+if __name__ == "__main__":
+    main()
